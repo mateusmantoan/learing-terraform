0cc7b9f110d684f42ed0bd0d36df19c6fcc33a14
mateusmantoan <mateusmantoan@gmail.com>
2021-12-16 22:18:32 +0000

    Add two first projects, enabling a 	new file:   learn-terraform-docker-container/main.tf 	new file:   learn-terraform-gcp/main.tf 	new file:   learn-terraform-gcp/outputs.tf 	new file:   learn-terraform-gcp/variables.tf

---
+++ learn-terraform-gcp/main.tf
@@ -0,0 +1,40 @@
+# config GCP project
+provider "google" {
+  credentials = file("/home/vagrant/tf-lab-335100-c1fb7b86cd18.json")
+  project     = "${var.project_id}"
+  region      = "${var.region}"
+}
+
+# create VM
+resource "google_compute_instance" "webserver" {
+  name         = "${var.name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.zone}"
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+
+# instalar apache2
+metadata_startup_script = "sudo apt update; sudo apt install apache2 -y; echo 'body count is in the house broder (MMA)' > /var/www/html/index.html"
+
+# Enable network
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+}
+
+# Create firewall rules
+resource "google_compute_firewall" "webfirewall" {
+  name    = "${var.firewall_name}"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = "${var.ports}"
+  }
+}
---
+++ learn-terraform-gcp/variables.tf
@@ -0,0 +1,39 @@
+variable "project_id" {
+  type    = string
+  default = "tf-lab-335100"
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "name" {
+  type = string
+  default = "apache-body-count"
+}
+
+variable "machine_type" {
+  type = string
+  default = "f1-micro"
+}
+
+variable "zone" {
+  type = string
+  default = "us-central1-c"
+}
+
+variable "image" {
+  type = string
+  default = "debian-cloud/debian-10"
+}
+
+variable "firewall_name" {
+  type = string
+  default = "webserver-firewall"
+}
+
+variable "ports" {
+  type = list
+  default = ["80"]
+}
